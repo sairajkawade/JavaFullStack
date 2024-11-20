7150a6fa47e26dd35b8e314d333db21597151b97
sairajkawade <kawadesairaj503@gmail.com>
2024-11-20 15:03:59 +0530

    Add new ArrayList program

---
+++ JavaPrograms/src/com/velocity/ArrayListDemo1.java
@@ -0,0 +1,16 @@
+package com.velocity;
+import java.util.*;
+public class ArrayListDemo1 {
+	
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<String>();
+		// add elements
+		list.add("ram");
+		list.add("shyam");
+		list.add("vikas");
+		list.add("yogesh");
+		System.out.println(list);
+	}
+
+}
